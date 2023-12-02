885642649340b8f6025737471c4a1a9d47ecf8c0
abel <vnponce8@gmail.com>
2023-12-02 10:49:38 -0600

    feat(fastAPI): implementing api router status

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 api = APIRouter()
 
-@api.get("/")
+
+@api.get("/", status_code=status.HTTP_200_OK)
 async def test():
-    return {"message": "hola"}
+    return {"no": "hola"}
