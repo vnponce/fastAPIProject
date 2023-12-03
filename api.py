e06b0840570894079020b9707b47ac86ca7bf2b0
abel <vnponce8@gmail.com>
2023-12-02 22:44:39 -0600

    feat(fastAPI): put method

--- api.py
+++ api.py
@@ -23,12 +23,30 @@ async def get_category(id: int):
 
 
 @api.post("/categories", response_model=ResponseSchema, status_code=status.HTTP_201_CREATED)
-def create_category(category: BaseCategorySchema):
+async def create_category(category: BaseCategorySchema):
     print(category)
     try:
         connect.execute(
             categories_model.insert().values({"name": category.name, "slug": slugify(category.name)}))
         return {"message": "crreated"}
-    except ValueError:
-        print(ValueError)
+    except:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@api.put("/categories/{id}", response_model=ResponseSchema, status_code=status.HTTP_200_OK)
+async def update_category(id: int, category: BaseCategorySchema):
+    data = connect.execute(categories_model.select().where(categories_model.c.id == id)).first()
+    if data:
+        connect.execute(categories_model.update().values(name=category.name, slug=slugify(category.name)))
+        return {"message": "updated"}
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Error")
+
+@api.delete("/categories/{id}", response_model=ResponseSchema, status_code=status.HTTP_200_OK)
+async def delete_category(id: int, category: BaseCategorySchema):
+    data = connect.execute(categories_model.select().where(categories_model.c.id == id)).first()
+    if data:
+        connect.execute(categories_model.delete())
+        return {"message": "deleted"}
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Error")
